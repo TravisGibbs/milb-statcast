48d6eda2771c502db07779ccb5b580cc9e1f0884
Travis Gibbs <travisgibbs.2019@gmail.com>
2023-05-12 19:41:00 -0400

    Small Fixes + Team Option

--- minor_statcast.py
+++ minor_statcast.py
@@ -1,13 +1,15 @@
 from concurrent.futures import as_completed
 import json
+from typing import List, Union
 import pandas as pd
 import requests
-import tqdm
+from tqdm import tqdm
 from itertools import chain
 from requests_futures.sessions import FuturesSession
 
-def game_pks_from_date(start_date: str, end_date: str, sport_id: str = "11") -> list[str]:
-    """ Wraps around the mlb stats api to gather gamePks to search statcast or other sources.
+def game_pks_from_date(start_dt: str, end_dt: str, sport_id: str = "11", team_id: str = None) -> List[str]:
+    """ 
+    Wraps around the mlb stats api to gather gamePks to search statcast or other sources.
 
     Args:
         start_date: String containing a date in yyyy-mm-dd format to start the search (inclusive)
@@ -16,17 +18,21 @@ def game_pks_from_date(start_date: str, end_date: str, sport_id: str = "11") -> 
     Returns:
         A list of strings of gamePks from the specified dates 
     """
-    content = json.loads(requests.get("https://statsapi.mlb.com/api/v1/schedule", params={"sportId": sport_id, "startDate": start_date, "endDate": end_date}).content)
+    content = json.loads(requests.get("https://statsapi.mlb.com/api/v1/schedule", params={"sportId": sport_id, "startDate": start_dt, "endDate": end_dt, "hydrate": "team", "teamId": team_id}).content)
     dates = content['dates']
     games = list(chain(*[date['games'] for date in dates]))
     # TODO build in option for statcast data and find way to identify statcast elligibility from schedule api (via venue?)
     return [game["gamePk"] for game in games if game['status']['codedGameState'] == "F"]
 
-def pitchdata_from_pks(pks: list[str], max_net_threads: int =  10) -> pd.DataFrame:
-    """ Uses multiple threads to gather relevant pitch by pitch data from game schedule
+def pitchdata_from_pks(pks: list[str], statcast_only = True, max_net_threads: int =  10) -> pd.DataFrame:
+    """ 
+    Uses multiple threads to gather relevant pitch by pitch data from game schedule
+
+    Note: Games with no statcast data have empty 
 
     Args:
         pks: A list of pk strings to search for stats
+        statcast_only: Only pull data from games with statcast data
         max_net_threads: An int representing the number of network threads to exsist at any given time
 
     Returns:
@@ -34,16 +40,18 @@ def pitchdata_from_pks(pks: list[str], max_net_threads: int =  10) -> pd.DataFra
     """
     session = FuturesSession(max_workers=max_net_threads)
     futures = [session.get("https://baseballsavant.mlb.com/gf", params={"game_pk": pk}) for pk in pks]
-    master_table = pd.DataFrame()
+    results = []
 
-    print("Gathering Games")
-    with tqdm(total=len(futures)) as pbar:
+    with tqdm(total=len(futures), desc="Gathering Data", unit="Game") as pbar:
         for completed_future in as_completed(futures):
-            res =json.loads(completed_future.result().content)
-            velo = pd.DataFrame(res['exit_velocity'])
-            table = pd.DataFrame(res['team_home']+res['team_away'])
-            table = table.merge(velo, on="play_id", how="left")
-            master_table = pd.concat([master_table, table])
+            res = json.loads(completed_future.result().content)
+            velo_data = pd.DataFrame(res['exit_velocity'])
+            if velo_data.size > 0 or not statcast_only:
+                PA_data = pd.DataFrame(res['team_home']+res['team_away'])
+                # Only perform join if velo has size (if statcast)
+                if velo_data.size > 0:
+                    PA_data = PA_data.merge(velo_data, on="play_id", how="left")
+                results.append(PA_data)
             pbar.update(1)
 
-    return master_table
+    return pd.concat(results)
